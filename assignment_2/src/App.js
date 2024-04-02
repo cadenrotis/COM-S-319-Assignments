78c6cc968048c18803fb65f59ff975e595cafb75
caden otis <cadenotis@gmail.com>
2024-04-02 00:43:10 -0500

    Updated all three views

--- assignment_2/src/App.js
+++ assignment_2/src/App.js
@@ -3,8 +3,8 @@ import React, { useState, useEffect } from "react";
 import Products from "./products.json";
 import { Link } from "react-router-dom";
 
-const render_products = (ProductsCategory, cart, cartTotal, { setCartQuantity, setCartTotal}) => {
-
+//this function will render the catalog of products and create buttons for users to add the products to their cart
+const render_products = (ProductsCategory, cart, { setCartQuantity }) => {
   //adding products to the cart
   const addToCart = (el) => {
     setCartQuantity([...cart, el]); //uses the spread operator
@@ -25,26 +25,17 @@ const render_products = (ProductsCategory, cart, cartTotal, { setCartQuantity, s
     }
   };
 
-  const cartItems = cart.map((el) => (
-    <div key={el.id}>
-      <img class=
-        "img-fluid" src={el.image} width={150} /> <br />
-      {el.title} <br />
-      ${el.price} <br />
-    </div>
-  ));
-
   //calculating the quantity of each item in the cart
   function howManyofThis(id) {
     let hmot = cart.filter((cartItem) => cartItem.id === id);
     return hmot.length;
-}
+  }
 
   return (
     <div className="category-section fixed">
       <div
         className="m-6 p-3 mt-10 ml-0 grid grid-cols-1 gap-y-10 gap-x-6 sm:grid-cols-2 lg:grid-cols-6 xl:gap-x-10"
-        style={{ maxHeight: "700px", overflowY: "scroll" }}
+        style={{ maxHeight: "550px", overflowY: "scroll"}}
       >
         {ProductsCategory.map((product, index) => (
           <div>
@@ -85,20 +76,21 @@ const render_products = (ProductsCategory, cart, cartTotal, { setCartQuantity, s
                 </p>
               </div>
             </div>
-            <div className="bg-gray-50 border border-gray-600 text-gray-900 text-sm rounded-lg p-1" style={{ display: "flex", justifyContent: "center", marginTop: "10px" }}>
-              <p> Quantity: {product.quantity} </p>
+            <div
+              className="bg-gray-50 border border-gray-600 text-gray-900 text-sm rounded-lg p-1"
+              style={{
+                display: "flex",
+                justifyContent: "center",
+                marginTop: "10px",
+              }}
+            >
+              <p> Quantity: {howManyofThis(product.id)} </p>
               <button
                 className="bg-gray-50 border border-gray-600 text-gray-900 text-sm rounded-lg p-1"
                 id="button"
                 type="button"
-                style={{ width: "40px", marginLeft: "100px" }}
-                onClick={() => {
-                  //setCartQuantity(cart + 1);
-                  setCartTotal(cartTotal + 1);
-                  product.quantity += 1;
-                  console.log("+ button is clicked");
-                }}
-                //onClick={() => addToCart(product)}
+                style={{ width: "30px", marginLeft: "40px" }}
+                onClick={() => addToCart(product)}
               >
                 <b>+</b>
               </button>
@@ -106,13 +98,8 @@ const render_products = (ProductsCategory, cart, cartTotal, { setCartQuantity, s
                 className="bg-gray-50 border border-gray-600 text-gray-900 text-sm rounded-lg p-1"
                 id="button"
                 type="button"
-                style={{ width: "40px" }}
-                onClick={() => {
-                  if (cart > 0) setCartQuantity(cart - 1);
-                  if (product.quantity > 0) product.quantity -= 1;
-                  console.log("- button is clicked");
-                }}
-                //onClick={() => removeFromCart(product)}
+                style={{ width: "30px" }}
+                onClick={() => removeFromCart(product)}
               >
                 <b>-</b>
               </button>
@@ -125,11 +112,16 @@ const render_products = (ProductsCategory, cart, cartTotal, { setCartQuantity, s
 };
 
 const App = () => {
+  // global states to handle the switching of views on the webpage
+  const [browseView, setBrowseView] = useState(true);
+  const [cartView, setCartView] = useState(false);
+  const [confirmView, setConfirmView] = useState(false);
+
   const [ProductsCategory, setProductsCategory] = useState(Products.phones); // ProductsCategory is used to display the products onto the webpage
   const [query, setQuery] = useState(""); // query is used to collect the input from the user when they use the search feature
 
-  const [cart, setCartQuantity] = useState([]); //cart is an array that'll store the products that we are interested in buying
-  const [cartTotal, setCartTotal] = useState(0); //cartTotal holds the total amount of items in the cart
+  const [cart, setCartQuantity] = useState([]); //cart is an array that'll store the products that the user is interested in buying
+  const [cartTotal, setCartTotal] = useState(0); //cartTotal holds the total price of items in the cart
 
   // when input is typed into the search bar, handleChange() updates ProductsCategory with the correct products to show based on the user's input
   const handleChange = (e) => {
@@ -156,41 +148,254 @@ const App = () => {
     setCartTotal(totalVal);
   };
 
-  return (
-    <div>
-      <div
-        className="py-10"
-        style={{ display: "flex", justifyContent: "center" }}
-      >
-        <input
-          className="border border-gray-30 text-gray-900 text-sm rounded-lg
+  //calculating the number of each product in the cart
+  function howManyofThis(id) {
+    let hmot = cart.filter((cartItem) => cartItem.id === id);
+    return hmot.length;
+  }
+
+  //checks for duplicate elements in the cart array
+  function checkForDuplicates(arr, element) {
+    let count = 0;
+
+    for (let i = 0; i < arr.length; i++) {
+      if (arr[i].id == element.id) {
+        count++;
+      }
+
+      if (count > 1) {
+        return true;
+      }
+    }
+
+    return false;
+  }
+
+  //checks to see if a product exists in the newly created cart array that doesn't contain any duplicate products
+  function productInCart(arr, element) {
+    for (let i = 0; i < arr.length; i++) {
+      if (arr[i].id == element.id) {
+        return true;
+      }
+    }
+
+    return false;
+  }
+
+  //will display the list of items in the users cart without the same items being listed more than once
+  function noDuplicatesCart() {
+    const newCart = []; //newCart will be a copy of cart without any duplicated items
+
+    for (let i = 0; i < cart.length; i++) {
+      if (checkForDuplicates(cart, cart[i]) && productInCart(newCart, cart[i])) {
+        console.log("There was a duplicate");
+        continue;
+      }
+
+      newCart.push(cart[i]);
+    }
+
+    for (let i = 0; i < newCart.length; i++) {
+      console.log(newCart[i].title);
+    }
+
+    //displays the items that are in the cart
+    const cartItems = newCart.map((el) => (
+      <div>
+        <div
+          className="py-8"
+          style={{ display: "flex", justifyContent: "left" }}
+          key={el.id}
+        >
+          <img
+            style={{ marginLeft: "5px", marginRight: "170px" }}
+            class="img-fluid"
+            src={el.image}
+            width={150}
+          />
+          <p style={{ marginRight: "250px", fontSize: "20px" }}>
+            {howManyofThis(el.id)}
+          </p>
+          <p style={{ fontSize: "20px" }}>${howManyofThis(el.id) * el.price}</p>
+        </div>
+        <p style={{ marginLeft: "5px", fontSize: "20px" }}>{el.title}</p>
+      </div>
+    ));
+
+    return cartItems;
+  }
+
+  //displays the browse view with all of the products
+  function BrowseView() {
+    return (
+      <div>
+        <div
+          className="py-8"
+          style={{ display: "flex", justifyContent: "center" }}
+        >
+          <input
+            className="border border-gray-30 text-gray-900 text-sm rounded-lg
 focus:ring-blue-500 focus:border-blue-500 block w-1/12 p-1 dark:bg-white-700
 dark:border-gray-300 dark:placeholder-gray-400 dark:text-black
 dark:focus:ring-blue-500 dark:focus:border-blue-500"
-          type="search"
-          value={query}
-          onChange={handleChange}
-        />
+            type="search"
+            value={query}
+            onChange={handleChange}
+          />
+          <button
+            className="bg-gray-50 border border-gray-600 text-gray-900 text-sm rounded-lg p-1"
+            onClick={() => {
+              setQuery("");
+              setProductsCategory(Products.phones);
+            }}
+          >
+            Clear Search
+          </button>
+          <button
+            className="bg-gray-50 border border-gray-600 text-gray-900 text-sm rounded-lg p-1"
+            style={{ marginLeft: "50px" }}
+            onClick={switchToCartView}
+          >
+            Checkout
+          </button>
+        </div>
+        <div className="ml-5 p-1 xl:basis-4/5">
+          {render_products(ProductsCategory, cart, { setCartQuantity })}
+        </div>
+      </div>
+    );
+  }
+
+  //displays the cart
+  function CartView() {
+    return (
+      <div>
+        <div
+          className="py-8"
+          style={{ display: "flex", justifyContent: "center" }}
+        >
+          <button
+            className="bg-gray-50 border border-gray-600 text-gray-900 text-sm rounded-lg p-1"
+            onClick={switchToBrowseView}
+          >
+            Return
+          </button>
+        </div>
+        <div style={{ display: "flex", justifyContent: "left" }}>
+          <p
+            style={{
+              marginLeft: "20px",
+              marginRight: "205px",
+              fontSize: "30px",
+            }}
+          >
+            Item
+          </p>
+          <p style={{ marginRight: "180px", fontSize: "30px" }}>Quantity</p>
+          <p style={{ fontSize: "30px" }}>Price</p>
+        </div>
+        <hr
+          style={{
+            height: "1px",
+            border: "none",
+            color: "#333",
+            backgroundColor: "#333",
+          }}
+        ></hr>
+        <div>{noDuplicatesCart()}</div>
+        <br />
+        <hr
+          style={{
+            height: "1px",
+            border: "none",
+            color: "#333",
+            backgroundColor: "#333",
+          }}
+        ></hr>
+        <div>
+          <p style={{ marginLeft: "570px", fontSize: "20px" }}>
+            Order total: ${cartTotal}
+          </p>
+        </div>
+        <hr
+          style={{
+            height: "1px",
+            border: "none",
+            color: "#333",
+            backgroundColor: "#333",
+          }}
+        ></hr>
+        <br />
+
+        <h1 style={{ fontSize: "30px" }}>
+          <b>Payment Information</b>
+        </h1>
+        <p>
+          Add input fields for user's name, email, card, address1, city, state,
+          zip
+        </p>
+        <br />
+
         <button
           className="bg-gray-50 border border-gray-600 text-gray-900 text-sm rounded-lg p-1"
-          onClick={() => {
-            setQuery("");
-            setProductsCategory(Products.phones);
-          }}
-        >
-          Clear Search
-        </button>
-        <Link to="/cartView"><button
-          className="bg-gray-50 border border-gray-600 text-gray-900 text-sm rounded-lg p-1"
           style={{ marginLeft: "50px" }}
+          onClick={switchToConfirmView}
         >
-          Checkout
+          Order
         </button>
-        </Link>
       </div>
-      <div className="ml-5 p-1 xl:basis-4/5">
-        {render_products(ProductsCategory, cart, cartTotal, { setCartQuantity, setCartTotal})}
+    );
+  }
+
+  //displays a screen for the user to confirm their order
+  function ConfirmView() {
+    return (
+      <div>
+        <div
+          className="py-8"
+          style={{ display: "flex", justifyContent: "center" }}
+        >
+          <button
+            className="bg-gray-50 border border-gray-600 text-gray-900 text-sm rounded-lg p-1"
+            style={{ marginLeft: "50px" }}
+            onClick={switchToBrowseView}
+          >
+            Go back to the catalog
+          </button>
+        </div>
+        <h1>This is the confirmation view</h1>
       </div>
+    );
+  }
+
+  // functions that handle the switching between the three views
+  const switchToBrowseView = () => {
+    if (browseView === false) setBrowseView(true);
+    else setBrowseView(false);
+
+    setCartView(false);
+    setConfirmView(false);
+  };
+  const switchToCartView = () => {
+    if (cartView === false) setCartView(true);
+    else setCartView(false);
+
+    setBrowseView(false);
+    setConfirmView(false);
+  };
+  const switchToConfirmView = () => {
+    if (confirmView === false) setConfirmView(true);
+    else setConfirmView(false);
+
+    setBrowseView(false);
+    setCartView(false);
+  };
+
+  return (
+    <div>
+      {browseView && <BrowseView />}
+      {cartView && <CartView />}
+      {confirmView && <ConfirmView />}
     </div>
   );
 };
--- assignment_2/src/index.js
+++ assignment_2/src/index.js
@@ -1,17 +1,12 @@
-import React from 'react';
-import ReactDOM from 'react-dom/client';
-import './index.css';
-import App from './App';
-import Shopping from "./Shopping.js";
-import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
+import React from "react";
+import ReactDOM from "react-dom/client";
+import "./index.css";
+import App from "./App";
 
-const root = ReactDOM.createRoot(document.getElementById('root'));
+const root = ReactDOM.createRoot(document.getElementById("root"));
 
 root.render(
-  <Router>
-    <Routes>
-      <Route path="/" element={<App />} />
-      <Route path="/cartView" element={<Shopping />} />
-    </Routes>
-  </Router>
+  <React.StrictMode>
+    <App/>
+  </React.StrictMode>
 );
